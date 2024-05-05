5529e7328cada7b9ce91867acdf7ef1007de54a5
hendisantika <hendisantika@yahoo.co.id>
2024-05-05 10:18:18 +0700

    Add DepositDataJdbc🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/transferservicehexagonalarchitecture/adapter/db/datajdbc/DepositDataJdbc.java
@@ -0,0 +1,16 @@
+package id.my.hendisantika.transferservicehexagonalarchitecture.adapter.db.datajdbc;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : transfer-service-hexagonal-architecture
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/5/24
+ * Time: 10:18
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DepositDataJdbc extends BaseDataJdbc, CrudRepository<DepositEntity, Long> {
+}
