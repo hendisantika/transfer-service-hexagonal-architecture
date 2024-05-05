631aaff8967e9a44b7f994f8a65a766c5219eb13
hendisantika <hendisantika@yahoo.co.id>
2024-05-05 09:26:51 +0700

    Create spring boot app 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/transferservicehexagonalarchitecture/TransferServiceHexagonalArchitectureApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.transferservicehexagonalarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferServiceHexagonalArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransferServiceHexagonalArchitectureApplication.class, args);
+    }
+
+}
