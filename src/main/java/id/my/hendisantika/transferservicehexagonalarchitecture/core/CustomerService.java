43314c56b4aee60a6915837b4e9745ec58ada296
hendisantika <hendisantika@yahoo.co.id>
2024-05-05 09:58:33 +0700

    Add CustomerService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/transferservicehexagonalarchitecture/core/CustomerService.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.transferservicehexagonalarchitecture.core;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : transfer-service-hexagonal-architecture
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/5/24
+ * Time: 09:58
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Transactional
+public class CustomerService {
+}
