948ea068e523360b25c2b1e6540cbb771f02baff
hendisantika <hendisantika@yahoo.co.id>
2024-05-05 10:08:32 +0700

    Add ErrorCode Enum🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/transferservicehexagonalarchitecture/core/model/ErrorCode.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.transferservicehexagonalarchitecture.core.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : transfer-service-hexagonal-architecture
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/5/24
+ * Time: 10:08
+ * To change this template use File | Settings | File Templates.
+ */
+public enum ErrorCode {
+    DATABASE_INTERNAL_ERROR,
+    CUSTOMER_DOES_NOT_EXIST,
+    UNKNOWN
+}
