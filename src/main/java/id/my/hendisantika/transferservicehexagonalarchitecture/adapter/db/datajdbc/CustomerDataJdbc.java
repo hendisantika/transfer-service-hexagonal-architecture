94802dbc2eadac53ac2cfc62854b7e393940bf3f
hendisantika <hendisantika@yahoo.co.id>
2024-05-05 10:17:58 +0700

    Add CustomerDataJdbc🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/transferservicehexagonalarchitecture/adapter/db/datajdbc/CustomerDataJdbc.java
@@ -0,0 +1,16 @@
+package id.my.hendisantika.transferservicehexagonalarchitecture.adapter.db.datajdbc;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : transfer-service-hexagonal-architecture
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/5/24
+ * Time: 10:17
+ * To change this template use File | Settings | File Templates.
+ */
+public interface CustomerDataJdbc extends BaseDataJdbc, CrudRepository<CustomerEntity, Long> {
+}
