6246f6f65dbb6d07811fd3602acb7771de2cd6df
hendisantika <hendisantika@yahoo.co.id>
2024-05-05 10:16:01 +0700

    Add PostgresDepositRepository🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/transferservicehexagonalarchitecture/adapter/db/PostgresDepositRepository.java
@@ -0,0 +1,35 @@
+package id.my.hendisantika.transferservicehexagonalarchitecture.adapter.db;
+
+import id.my.hendisantika.transferservicehexagonalarchitecture.core.model.Deposit;
+import id.my.hendisantika.transferservicehexagonalarchitecture.core.port.DepositRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : transfer-service-hexagonal-architecture
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/5/24
+ * Time: 10:15
+ * To change this template use File | Settings | File Templates.
+ */
+@AllArgsConstructor
+@Slf4j
+public class PostgresDepositRepository implements DepositRepository {
+
+    private DepositDataJdbc depositDataJdbc;
+
+    @Override
+    public Deposit save(Deposit deposit) {
+        try {
+            var depositEntity = DepositEntity.fromDeposit(deposit);
+            var savedDepositEntity = depositDataJdbc.save(depositEntity);
+            return savedDepositEntity.toDeposit();
+        } catch (RuntimeException e) {
+            log.warn("Cannot save deposit={}", deposit);
+            throw new RepositoryException("Cannot save deposit.", e);
+        }
+    }
+}
