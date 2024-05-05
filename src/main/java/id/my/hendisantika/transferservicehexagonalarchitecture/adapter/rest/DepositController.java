7ea3a12db4a9533791c69920547030529531a40c
hendisantika <hendisantika@yahoo.co.id>
2024-05-05 10:13:16 +0700

    Add addDeposit in DepositController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/transferservicehexagonalarchitecture/adapter/rest/DepositController.java
+++ src/main/java/id/my/hendisantika/transferservicehexagonalarchitecture/adapter/rest/DepositController.java
@@ -4,7 +4,11 @@ import id.my.hendisantika.transferservicehexagonalarchitecture.core.port.Deposit
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +30,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class DepositController {
 
     private final DepositService depositService;
+
+    @PostMapping(path = "/deposit", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> add(@RequestBody DepositDto dto) {
+        depositService.add(dto.toDomain());
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(dto.requestUid());
+    }
 }
