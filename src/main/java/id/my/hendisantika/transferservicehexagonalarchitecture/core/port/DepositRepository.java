ed93bb5d648900b55413a20a9355743743ddccea
hendisantika <hendisantika@yahoo.co.id>
2024-05-05 10:06:33 +0700

    Add DepositRepository🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/transferservicehexagonalarchitecture/core/port/DepositRepository.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.transferservicehexagonalarchitecture.core.port;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : transfer-service-hexagonal-architecture
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/5/24
+ * Time: 10:06
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DepositRepository {
+    Deposit save(Deposit deposit);
+}
