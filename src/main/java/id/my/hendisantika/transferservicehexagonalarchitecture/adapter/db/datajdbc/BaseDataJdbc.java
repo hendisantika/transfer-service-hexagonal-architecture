807e696cf2a2118960baf71ab7fc828c77469b8a
hendisantika <hendisantika@yahoo.co.id>
2024-05-05 10:17:35 +0700

    Add BaseDataJdbc🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/transferservicehexagonalarchitecture/adapter/db/datajdbc/BaseDataJdbc.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.transferservicehexagonalarchitecture.adapter.db.datajdbc;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : transfer-service-hexagonal-architecture
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/5/24
+ * Time: 10:17
+ * To change this template use File | Settings | File Templates.
+ */
+//Use this to get package of Spring Data JDBC
+public interface BaseDataJdbc {
+}
